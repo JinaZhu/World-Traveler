5ad5f3b3cf0ae62aed4511ba216de78669212351
vagrant <vagrant@vagrant.vm>
2020-02-28 20:21:38 +0000

    got country name with lat and long

--- client/src/DisplayMap.js
+++ client/src/DisplayMap.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+import NodeGeocoder from "node-geocoder";
 
 const apiKey = 'AIzaSyAGIgU3ILBZtHca1RACPDe30eGGMQAMtHw'
+const options = {
+    provider: "google",
+    apiKey: apiKey
+};
+
+const geocoder = NodeGeocoder(options);
 const mapStyles = {
     height: '80%',
     width: '100%'
@@ -24,36 +31,28 @@ const mapStyles = {
 
 
 class DisplayMap extends Component {
-    // constructor() {
-    //     super()
-    //     this.state = {
-    //         lat: '',
-    //         long: ''
-    //     }
+    constructor() {
+        super()
+        this.state = {
+            lat: '',
+            long: ''
+        }
 
-    //     this.handleMapClick = this.handleMapClick.bind(this)
-    // }
-
-
+        this.handleMapClick = this.handleMapClick.bind(this)
+    }
 
     handleMapClick(mapProps, map, clickEvent) {
-        // const { latLng } = clickEvent
-        // const lat = latLng.lat()
-        // const lng = latLng.lng()
 
-        // const  country = geoCode({lat, lng})
+        const { latLng } = clickEvent;
 
-        // const  xhr = $.post(/countriesinfo, {country})
+        const lat = latLng.lat();
+        const lon = latLng.lng();
+        this.setState({ lat, lng: lon });
 
-        // xhr.hr.done
+        geocoder.reverse({ lat, lon }, function (err, res) {
+            console.log("res", res[0].country);
+        });
 
-        // console.log('this', this)
-
-        // this.setState({
-        //     lat, lng
-        // })
-        console.log('clickEvent', clickEvent.latLng.lat())
-        console.log('clickEvent', clickEvent.latLng.lng())
     }
 
     render() {
