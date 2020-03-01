98909c67ae51b7fe48ad8d3dc40114eeff77cb7b
vagrant <vagrant@vagrant.vm>
2020-03-01 06:33:15 +0000

    added photo carousel

--- client/src/CountryInfo.js
+++ client/src/CountryInfo.js
@@ -1,61 +1,84 @@
 import React from 'react';
 import './App.css';
 import $ from "jquery"
-import { Button } from 'reactstrap'
+import { Button, UncontrolledCarousel } from 'reactstrap'
 
 
-const CountryInfo = (props) => {
-    const { country, isLoading } = props;
+
+const CountryInfo = ({ country, isLoading }) => {
+    if (!isLoading && !country) {
+        return null
+    }
+
+    if (isLoading) {
+        return <img style={{ width: "50%", height: "50%" }} alt="loading..." src="https://media0.giphy.com/media/8F94rv33nxAFvNEc4H/source.gif" />
+    }
 
     const handleCountrySave = (e) => {
-        if (country !== undefined) {
-            const xhr = $.post('/save', {
-                'country': country.country_info.countryName,
-                'imgUrl': country.place_photos[0]
-            })
-            xhr.done((data) => {
-                window.alert("Country Saved!")
-            })
-            xhr.fail((error) => {
-                console.log('error', error)
-            })
-        }
+        const xhr = $.post('/save', {
+            'country': country.country_info.countryName, // country exists at this point, so no need to handle when country is undefined
+            'imgUrl': country.place_photos[0]
+        })
+        xhr.done((data) => {
+            window.alert("Country Saved!")
+        })
+        xhr.fail((error) => {
+            console.log('error', error)
+        })
+
     }
+
+
+
+
+    console.log('isLoading', isLoading)
+    console.log('country', country)
+
+    const countryPhotoList = country.place_photos.map((reference, index) => {
+        return {
+            'src': `https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=AIzaSyAGIgU3ILBZtHca1RACPDe30eGGMQAMtHw`,
+            'altText': index + 1,
+            'caption': `Slide ${index + 1}`,
+            'header': `Slide ${index + 1} Header`,
+            'key': index
+        }
+    })
+
+    console.log(countryPhotoList)
     return (
 
         <div>
             <section id="display-country">
 
-                {isLoading && <img style={{ width: "50%", height: "50%" }} alt="loading..." src="https://media0.giphy.com/media/8F94rv33nxAFvNEc4H/source.gif" />}
-                {country &&
+                {/* {isLoading && <img style={{ width: "50%", height: "50%" }} alt="loading..." src="https://media0.giphy.com/media/8F94rv33nxAFvNEc4H/source.gif" />} */}
+                <div>
+                    <h3>Country Information</h3>
+                    <Button outline color='primary' onClick={handleCountrySave}>Save</Button>
+
+                    <dl>
+                        <dt>Name: {country.country_info.countryName} </dt>
+
+                        <dt>Currency: {country.currency}</dt>
+
+                        <dt>Lanuage: {country.language}</dt>
+
+                        <dt>Visa: {country.country_info.visa}</dt>
+
+                        <dt>Vaccination/s: {country.country_info.vaccination}</dt>
+
+                        <dt>Monthly Average temperature: {country.country_info.temperatures} for {country.country_info.city_temp}</dt>
+
+                        <dt>Advisor Score: {country.advisor_score} to learn more {country.learn_more_advisory}</dt>
+
+                        <dt>Popular Cities: <ul>{country.popular_cities.map((city, index) => {
+                            return <li key={index}>{city}</li>
+                        })}</ul></dt>
+                    </dl>
                     <div>
-                        <h3>Country Information</h3>
-                        <Button outline color='primary' onClick={handleCountrySave}>Save</Button>
-
-                        <dl>
-                            <dt>Name: {country.country_info.countryName} </dt>
-
-                            <dt>Currency: {country.currency}</dt>
-
-                            <dt>Lanuage: {country.language}</dt>
-
-                            <dt>Visa: {country.country_info.visa}</dt>
-
-                            <dt>Vaccination/s: {country.country_info.vaccination}</dt>
-
-                            <dt>Monthly Average temperature: {country.country_info.temperatures} for {country.country_info.city_temp}</dt>
-
-                            <dt>Advisor Score: {country.advisor_score} to learn more {country.learn_more_advisory}</dt>
-
-                            <dt>Popular Cities: <ul>{country.popular_cities.map((city, index) => {
-                                return <li key={index}>{city}</li>
-                            })}</ul></dt>
-                        </dl>
-                        <div> {country.place_photos.map((reference, index) => {
-                            return <img key={index} alt="country" src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=AIzaSyAGIgU3ILBZtHca1RACPDe30eGGMQAMtHw`} />
-                        })}</div>
+                        <UncontrolledCarousel items={countryPhotoList} />
                     </div>
-                }
+                </div>
+
             </section>
         </div>
     )
