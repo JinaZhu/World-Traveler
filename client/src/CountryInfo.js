7a375efadb438198e11be6974bd885a5f96f192f
vagrant <vagrant@vagrant.vm>
2020-03-09 21:51:20 +0000

    change background color

--- client/src/CountryInfo.js
+++ client/src/CountryInfo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import $ from "jquery"
 import { Button } from 'reactstrap'
-import { Info, FilteredPhoto } from './styled'
+import { Info, FilteredPhoto, StyledH3, ButtonAlign, PhotoAlign } from './styled'
 
 
 
@@ -45,32 +45,35 @@ const CountryInfo = ({ country, isLoading }) => {
 
     }
 
-    console.log(country)
-
     return (
         <Info>
 
             {/* {isLoading && <img style={{ width: "50%", height: "50%" }} alt="loading..." src="https://media0.giphy.com/media/8F94rv33nxAFvNEc4H/source.gif" />} */}
-            <h3>{country.country_info.countryName}</h3>
-            <div>
-                <Button outline color='primary' onClick={handleCountrySave}>Save</Button>
-                <Button outline color='primary' onClick={handleVisitedCountry}>Visited</Button>
-                <div>{country.place_photos.map((reference, index) => {
+            <img style={{ display: "block", marginLeft: "45%", marginRight: "auto", marginTop: "5%" }} src="https://surgicorps.org/wp-content/uploads/2018/02/watercolor-world-map.png" alt="map" width="100" length="100" />
+            <StyledH3>{country.country_info.countryName}</StyledH3>
+            <ButtonAlign>
+                <Button outline color='dark' onClick={handleCountrySave}>Save</Button>
+                <Button outline color='dark' onClick={handleVisitedCountry}>Visited</Button>
+            </ButtonAlign>
+            {/* 
+            <PhotoAlign>
+                {country.place_photos.map((reference, index) => {
                     return <FilteredPhoto key={index} alt="country"
                         src={`https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=${reference}&key=AIzaSyAGIgU3ILBZtHca1RACPDe30eGGMQAMtHw`}
-                        width="250" height="250" />
+                        width="200" height="200" />
                 })}
-                </div>
-            </div>
+            </PhotoAlign> */}
 
             <div>
                 <div>Lanuage: {country.language}</div>
                 <div>Vaccination/s: {country.country_info.vaccination}</div>
                 <div>Visa: {country.country_info.visa}</div>
                 <div>Advisor Score: {country.advisor_score} to learn more {country.learn_more_advisory}</div>
-            </div>
-            <div>
-                <div>Currency: {country.currency}</div>
+
+                <div>
+                    <i class="fas fa-coins"></i>
+                    Currency: {country.currency}
+                </div>
                 <div>Average Price: ${country.country_info.avg_price}</div>
             </div>
             <div>
--- client/src/styled.js
+++ client/src/styled.js
@@ -15,20 +15,49 @@ export const Slider = styled.div`
     left: 0;
     width: 100%;
     height: 125vh;
-    background: linear-gradient(to top, #E5DACE, #6D7973);
+    background: linear-gradient(to top, white, #B2B2A2, #6D7973, #B2B2A2, #E5DACE, #E7BAA0, #e7c9a9);
     z-index: -1;
 `;
 
+
 export const Info = styled.div`
-    margin: 0 10%;
+    margin: 0 7%;
 `;
+
 
 export const FilteredPhoto = styled.img`
     width: 250;
     height: 250;    
     opacity: 0.85;
     filter: grayscale(10%) sepia(30%) saturate(1.5) brightness(1.1) contrast(1); 
+    margin: 1%; 
 `
+
+export const StyledH3 = styled.h3`
+    display: flex; 
+    margin: 5% 40; 
+    color: #3F3931; 
+    font-size: 25px; 
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(to left, white, #e7c9a9, white);
+`
+
+export const ButtonAlign = styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    margin: 3% 50%;
+`
+
+export const PhotoAlign = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: row wrap;
+    background: linear-gradient(to top, white, #E7BAA0, #E5DACE, white);
+`
+
 
 
 
