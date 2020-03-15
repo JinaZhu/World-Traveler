2e4685e815a63683bd2f09fd5c04a1d046e001c4
Jina Zhu <jinazhu87@gmail.com>
2020-03-15 14:08:28 -0700

    fixed homepage refresh bug

--- client/src/Home.js
+++ client/src/Home.js
@@ -25,7 +25,7 @@ const Home = ({ user }) => {
     useEffect(() => {
         tl.fromTo(slider, 1.2, { y: "-100%" }, { y: "0%", ease: Power2.easeInOut }, "-=1.2")
             .fromTo(mapSlide, 1.2, { x: "-110%" }, { x: "0%", ease: Power2.easeInOut }, "-=1.2")
-    }, [tl])
+    }, [])
 
 
     function handleGenerateClick() {
