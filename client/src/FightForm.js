8ec41e820eb1c0503190906374a1b505473d72da
Jina Zhu <jinazhu87@gmail.com>
2020-04-25 20:10:41 -0700

    created form for flight search

---
+++ client/src/FightForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FightForm() {
+  return (
+    <form>
+      <input type="text">To</input>
+      <input type="text">From</input>
+      <input type="date"></input>
+      <button type="submit">Find</button>
+    </form>
+  );
+}
+
+export default FightForm;
